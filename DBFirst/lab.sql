27aa4d04f17266708d9654439a8db16cd28745fc
ytenen <152442891+ytenen@users.noreply.github.com>
2024-03-08 11:32:22 +0300

    lab.sql

--- DBFirst/lab.sql
+++ DBFirst/lab.sql
@@ -1,19 +1,19 @@
 \gset
-DROP TABLE IF EXISTS monolit, stars, humanity, repeater, signals, rest;
+DROP TABLE IF EXISTS monolit, stars, repeater, signals, rest;
 
 
 CREATE TABLE monolit(
     id SERIAL PRIMARY KEY,
-    monolit_name VARCHAR(256),
+    monolit_name VARCHAR(256) NOT NULL UNIQUE,
     condition_of_monolit VARCHAR(256),
     time_of_signal VARCHAR(32) NOT NULL
 );
 
 
 CREATE TABLE signals(
-    id SERIAL PRIMARY KEY,
-    signal_name VARCHAR(256),
-    monolit_id int NOT NULL REFERENCES monolit(id),
+    id SERIAL PRIMARY KEY ,
+    signal_name VARCHAR(256) NOT NULL UNIQUE,
+    monolit_id int NOT NULL,
     direction VARCHAR(256),
     time_of_send VARCHAR(32) NOT NULL
 );
@@ -21,38 +21,31 @@ CREATE TABLE signals(
 
 CREATE TABLE repeater(
     id SERIAL PRIMARY KEY,
-    repeater_name VARCHAR(256),
+    repeater_name VARCHAR(256) NOT NULL UNIQUE,
     signal_id int REFERENCES signals(id),
     surface VARCHAR(256),
-    time_of_broadcast VARCHAR(32)
+    time_of_broadcast VARCHAR(32) NOT NULL,
+    FOREIGN KEY(signal_id) REFERENCES signals(id)
 );
-
-
-CREATE TABLE stars(
-    id SERIAL PRIMARY KEY,
-    stars_name VARCHAR(256),
-    signal_id int REFERENCES signals(id),
-    repeater_id int REFERENCES repeater(id),
-    time_of_signal_arrival VARCHAR(256),
-    surface VARCHAR(256)
-);
-
 
 CREATE TABLE rest(
-    id SERIAL PRIMARY KEY,
-    rest_name VARCHAR(256),
-    stars_id int REFERENCES stars(id),
+    id SERIAL PRIMARY KEY ,
+    rest_name VARCHAR(256) NOT NULL UNIQUE,
     duration VARCHAR(256),
     reason VARCHAR(256)
 );
 
-
-CREATE TABLE humanity(
+CREATE TABLE stars(
     id SERIAL PRIMARY KEY,
-    rest_id int REFERENCES rest(id),
-    humanity_name VARCHAR(256),
-    time_of_signal_reception VARCHAR(256)
+    stars_name VARCHAR(256) NOT NULL UNIQUE,
+    rest_id int REFERENCES rest(id) ON DELETE CASCADE,
+    signal_id int REFERENCES signals(id) ON DELETE CASCADE,
+    repeater_id int REFERENCES repeater(id) ON DELETE CASCADE,
+    time_of_signal_arrival VARCHAR(256) NOT NULL,
+    surface VARCHAR(256)
 );
+
+
 
 
 INSERT INTO monolit (monolit_name, condition_of_monolit, time_of_signal) VALUES 
@@ -61,26 +54,21 @@ INSERT INTO monolit (monolit_name, condition_of_monolit, time_of_signal) VALUES
 
 INSERT INTO signals (signal_name, monolit_id, direction, time_of_send)
 VALUES
-('сигнал', 1, 'куда-то к звездам', '22:12:2023 12:43:32'),
-('сигнал', 1, 'куда-то к звездам', '22:12:2023 13:43:32');
+('сигнал1', 1, 'куда-то к звездам', '22:12:2023 12:43:32'),
+('сигнал2', 1, 'куда-то к звездам', '22:12:2023 13:43:32');
 
 
 INSERT INTO repeater (repeater_name, signal_id, surface, time_of_broadcast)
 VALUES
 ('ретрансляционное устройство', 1, 'где-то близ Сатурна', 'долгие годы');
 
-
-INSERT INTO stars (stars_name, signal_id, repeater_id, time_of_signal_arrival, surface)
+INSERT INTO rest (rest_name, duration, reason)
 VALUES
-('звезды', 1, 1, 'долгие годы', 'в далекой далекой галактике'),
-('звезды', 2, NULL, 'долгие годы', 'в далекой далекой галактике');
+('передышка', 'десятки, а вернее всего сотни лет', 'большое расстояние до звезд');
 
 
-INSERT INTO rest (stars_id, rest_name, duration, reason)
+INSERT INTO stars (rest_id, stars_name, signal_id, repeater_id, time_of_signal_arrival, surface)
 VALUES
-(1, 'передышка', 'десятки, а вернее всего сотни лет', 'большое расстояние до звезд');
+(1, 'звезды1', 1, 1, 'долгие годы', 'в далекой далекой галактике'),
+(1, 'звезды2', 2, NULL, 'долгие годы', 'в далекой далекой галактике');
 
-
-INSERT INTO humanity (rest_id, humanity_name, time_of_signal_reception)
-VALUES
-(1, 'человечество', 'десятки, а вернее всего сотни лет');
